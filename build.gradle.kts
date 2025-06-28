23742e6dc4846af9942a5c4c314f00b80d87b09d
Serhii Shvets <wert2all@gmail.com>
2025-06-28 23:25:50 +0300

    chore: add ktlint support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,23 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android = true // Enable Android-specific linting rules
+        ignoreFailures = false // Fail the build if KtLint finds any issues
+        reporters {
+            reporter(ReporterType.PLAIN) // Output KtLint results in plain text format
+            reporter(ReporterType.HTML) // Output KtLint results in HTML format
+        }
+    }
 }
